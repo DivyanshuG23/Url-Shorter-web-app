a7851fa269881cbad8f3694de344b4683d8a8e6d
Divyanshu Gangwar <divyanshugangwar663@gmail.com>
2026-04-01 10:04:50 +0530

    gitignore added

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import mongoose from "mongoose";
+import {shortUrl,getOriginalUrl} from "./Controllers/url.js";
+const app=express();
+app.use(express.urlencoded({extended:true}));
+mongoose
+    .connect("mongodb://divyanshugangwar663_db_user:6YHDKPcFi83MIR0x@ac-zrffpz9-shard-00-00.j9riwxt.mongodb.net:27017,ac-zrffpz9-shard-00-01.j9riwxt.mongodb.net:27017,ac-zrffpz9-shard-00-02.j9riwxt.mongodb.net:27017/?ssl=true&replicaSet=atlas-w63j91-shard-0&authSource=admin&appName=Cluster0",
+{
+    dbName:"node1_master"
+}
+)
+.then(()=>
+    console.log("mangodb connected..!")).catch((err)=>console.log(err));
+
+//rendering the ejs file
+app.get('/',(req,res)=>{
+    res.render("index.ejs",{shortUrl:null})
+})
+//shorting url logic
+app.post('/short', shortUrl) 
+
+//redirect to original url using short code :- dynamic routing
+app.get("/:shortCode",getOriginalUrl);
+    
+
+
+const port=1000;
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`);
+    
+})
